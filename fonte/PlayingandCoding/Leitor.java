673650f0c56fbb28efe3ea29a523b613155778fd
henriqueherrera <henriqueherrera9@gmail.com>
2019-04-25 22:17:59 -0300

    agora instancia as classes sempre que cria uma nova variavel, antes dava um erro que ficava add varias variaveis iguais

--- fonte/PlayingandCoding/Leitor.java
+++ fonte/PlayingandCoding/Leitor.java
@@ -7,12 +7,13 @@ public class Leitor
 
 	private String arquivo; //Lê o arquivo para ser Interpretado
 
-	private Bool bool = new Bool();
-	private Int inteiros = new Int();
+	private Bool bool;
+
+	private Int inteiros;
     	
-	private Float floats = new Float(); // Instancia os floats? Comentário lixo
+	private Float floats; // Instancia os floats? Comentário lixo
 	
-	private Char chars = new Char();
+	private Char chars;
 
 	public static boolean laco = true;
 	//Construtor
@@ -39,24 +40,27 @@ public class Leitor
 					//chama as funções aqui para armazenamento
 					
 					if (linha.substring(0,3).equals("int"))
-					{
+					{	
+						inteiros = new Int();
 						inteiros.verificador(linha);
                                 		//System.out.println(linha);
 					}
                     //se encontrar float no inicio da linha, chama o verificador de floats
                     			else if (linha.substring(0,5).equals("float"))
 					{
+						floats = new Float();
 						floats.verificador(linha);
                                 		//System.out.println(linha);
 					}
 					else if (linha.substring(0,4).equals("char"))
 					{
-
+						chars = new Char();
 						chars.verificador(linha);
 					}
 
 					else if (linha.substring(0,4).equals("bool"))
 					{
+						bool = new Bool();
 						bool.verificador(linha);
 					}
 				}
