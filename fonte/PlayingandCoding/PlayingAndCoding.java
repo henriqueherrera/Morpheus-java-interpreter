cc01ff224db094efb68aac59685ea5d3de6b92a8
henriqueherrera <henriqueherrera9@gmail.com>
2019-05-16 00:47:57 -0300

    tem input para vetores booleanos agr

--- fonte/PlayingandCoding/PlayingAndCoding.java
+++ fonte/PlayingandCoding/PlayingAndCoding.java
@@ -8,7 +8,6 @@ public class PlayingAndCoding
 		{
 			Leitor interpretador = new Leitor(args[0]);
 			interpretador.reader();
-			//System.out.println(Bool.vetoresArmazenados);
 		}
 		catch(Exception e)
 		{
