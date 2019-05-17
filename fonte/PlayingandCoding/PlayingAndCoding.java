9550a24b7f2619a85d02bcebb36f9664e9f0e183
PabloLodi <lodiricardopablo@gmail.com>
2019-05-17 02:16:20 -0300

    o catch da Main não jogava toda a exception no terminal. comentei ela pra ficar mais facil de debuggar

--- fonte/PlayingandCoding/PlayingAndCoding.java
+++ fonte/PlayingandCoding/PlayingAndCoding.java
@@ -4,16 +4,16 @@ public class PlayingAndCoding
 	//inicio do método main
 	public static void main(String[] args) 
 	{
-		try
-		{
-			Leitor interpretador = new Leitor(args[0]);
-			interpretador.reader();
+		//try
+		//{
+		Leitor interpretador = new Leitor(args[0]);
+		interpretador.reader();
 			//System.out.println(Int.vetoresArmazenados);
-		}
-		catch(Exception e)
-		{
-			System.out.println("algo de errado nao está certo no código "+e);
-		}
+		//}
+		//catch(Exception e)
+		//{
+		//	System.out.println("algo de errado nao está certo no código "+e);
+		//}
 	}
 	//fim do método main
 }
