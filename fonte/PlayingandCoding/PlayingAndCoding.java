99cfd21e7171d4a1b9b7ea2d62cb71402e8f7247
henriqueherrera <henriqueherrera9@gmail.com>
2019-05-17 11:30:38 -0300

    arrumei as merda que eu fiz

--- fonte/PlayingandCoding/PlayingAndCoding.java
+++ fonte/PlayingandCoding/PlayingAndCoding.java
@@ -5,31 +5,17 @@ public class PlayingAndCoding
 	//inicio do método main
 	public static void main(String[] args) 
 	{
-<<<<<<< HEAD
 		try
 		{
 			Leitor interpretador = new Leitor(args[0]);
 			long tempoInicial = System.currentTimeMillis();
 			interpretador.reader();
-			System.out.println("tempo de execução: " + (System.currentTimeMillis() - tempoInicial)*0.001f +" segundos");
 			//System.out.println(Int.variaveisArmazenadas);
 		}
 		catch(Exception e)
 		{
 			System.out.println("algo de errado nao está certo no código "+e);
 		}
-=======
-		//try
-		//{
-		Leitor interpretador = new Leitor(args[0]);
-		interpretador.reader();
-			//System.out.println(Int.vetoresArmazenados);
-		//}
-		//catch(Exception e)
-		//{
-		//	System.out.println("algo de errado nao está certo no código "+e);
-		//}
->>>>>>> 7a1aca48440b3e0039fed5faca3ff7e5e957a9bd
 	}
 	//fim do método main
 }
