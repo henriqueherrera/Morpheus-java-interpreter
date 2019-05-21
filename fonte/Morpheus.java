58ece0f390c86015f941535378c5762dcb95d4d7
PabloLodi <lodiricardopablo@gmail.com>
2019-05-21 20:51:03 -0300

    Morpheus.java comentado

--- fonte/Morpheus.java
+++ fonte/Morpheus.java
@@ -4,16 +4,19 @@ public class Morpheus
 	//inicio do método main
 	public static void main(String[] args) 
 	{
+		// Tenta executar os comandos seguintes
 		try
 		{
-			Leitor interpretador = new Leitor(args[0]);
-			long tempoInicial = System.currentTimeMillis();
-			interpretador.reader();
+			Leitor interpretador = new Leitor(args[0]); // Instancia o Leitor
+			long tempoInicial = System.currentTimeMillis(); // Grava o tempo do início do programa na variável
+			interpretador.reader(); // Inicia a leitura do arquivo
+			// Imprime o tempo de execução ao finalizar o código com sucesso
 			System.out.println("tempo de execução: " + (System.currentTimeMillis() - tempoInicial)*0.001f +" segundos");
 		}
-
+		// Caso ocorra um erro
 		catch(Exception e)
 		{
+			// É impresso o erro na tela
 			System.out.println("Error: na linha ("+ErrosNaCompilacao.numeroDaLinha+") verifique se desta linha para trás, está tudo de acordo com a sintaxe de Morpheus");
 		}
 	}
