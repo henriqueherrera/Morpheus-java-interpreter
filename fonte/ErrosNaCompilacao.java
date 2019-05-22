89230f361868922a8cfaaffb38f2b27a228575fc
Matheus Slama <matheus_slama@hotmail.com>
2019-05-21 22:02:53 -0300

    Adição de novas frases nos erros

--- fonte/ErrosNaCompilacao.java
+++ fonte/ErrosNaCompilacao.java
@@ -12,22 +12,22 @@ class ErrosNaCompilacao
 		switch(x)
 		{
 			case 0:
-                		System.out.println("(001) Erro na linha "+numeroDaLinha+" Falta colocar ';'" );
+                		System.out.println("(001) Tudo que tem um começo tem um fim.(Oráculo) \n Erro na linha "+numeroDaLinha+" Falta colocar ';' \n" );
 				Leitor.laco = false;
 				break;
 			
 			case 1:
 				
-                		System.out.println("(002) Erro na linha "+numeroDaLinha+" nenhum '"+tipo+"' identificado" );
+                		System.out.println("(002) Eles são todos e não são ninguém.(Morpheus) \n Erro na linha "+numeroDaLinha+" nenhum '"+tipo+"' identificado\n" );
 				Leitor.laco = false;
 				break;
 
 			case 2:
-                		System.out.println("(Game over) Erro na linha "+numeroDaLinha+" caractere '"+caractere+"' não é um inteiro");
+                		System.out.println("(003) Há uma grande diferença entre saber o caminho e percorrer o caminho.(Morpheus) \n Erro na linha "+numeroDaLinha+" caractere '"+caractere+"' não é um inteiro\n");
 				Leitor.laco = false;
 				break;
 			case 3:
-				System.out.println("(Game over) Nenhum nome de várivel encontrada na linha "+numeroDaLinha);
+				System.out.println("(004) Não pense que é , Saiba que é .(Morpheus) \n Nenhum nome de várivel encontrada na linha "+numeroDaLinha+"\n");
 				Leitor.laco = false;
 				break;
 				
