4f5132e7339256920b7e6e88b5f18f526d3e1ed1
PabloLodi <lodiricardopablo@gmail.com>
2019-05-22 00:54:47 -0300

    add classe Leitor comentada

--- fonte/Leitor.java
+++ fonte/Leitor.java
@@ -2,89 +2,90 @@ import java.io.*;
 //inicio da classe Leitor
 class Leitor extends Tools
 {
-	private String arquivo; //Lê o arquivo para ser Interpretado
+	private String arquivo; // Le o arquivo para ser Interpretado.
 
-	private LePrimitivos lePrimitivos = new LePrimitivos();
+	private LePrimitivos lePrimitivos = new LePrimitivos(); // Instancia o objeto que controla a leitura dos tipos primitivos.
 	
-	public static boolean laco = true; //enquanto tudo estiver ocorrendo de maneira prevista
-					//o laco de repeticao continua trocando de linha
+	public static boolean laco = true; // Enquanto tudo estiver ocorrendo de maneira prevista
+					// o laco de repeticao continua trocando de linha.
 	//Construtor
-	public Leitor(String arq)
+	public Leitor(String arq) // Construtor que define o caminho do arquivo.
 	{
+		// Aceita o arquivo se ele possuir a extensão .mp
 		if(arq.contains(".mp"))
 		{
 			this.setArquivo(arq);
 		}
+		// Caso contrário, imprime uma mensagem de erro.
 		else
 		{
-			System.out.println("Para interpretar um código é preciso colocar na extesão .mp");
+			System.out.println("Para interpretar um código é preciso colocar na extensão .mp");
 		}
 	}
 
-	public Leitor()
+	public Leitor() // PRA QUE SERVE ESSE CONSTRUTOR SOZINHO?
 	{
 	}
 
-	//inicio do método reader
+	//inicio do método reader.
 	public void reader() 
 	{
+		// Tenta rodar os comandos seguintes:
 		try
 		{
-			BufferedReader buffRead = new BufferedReader(new FileReader (this.getArquivo()));
+			BufferedReader buffRead = new BufferedReader(new FileReader (this.getArquivo())); // Le o arquivo do código.
 
-            String linha = "";
-
+            String linha = ""; // Define a variável que armazena cada linha lida.
+		    // Enquanto tudo estiver rodando corretamente, o laço permanece rodando.
             while(laco)
             {
-			
-				linha = buffRead.readLine();
-                        
-				if(linha != null)
+				linha = buffRead.readLine(); // Guarda a linha atual na variável.
+           
+				if(linha != null) // Se a linha não for nula:
                 {
+                    // Se a linha for diferente de zero e não estiver comentada:
 					if((linha.length() != 0 ) && (!linha.contains("//")) )
 					{
-						linha = linha.trim();
-						lePrimitivos.idLinha(linha,0); //chama o metodo para ler as variaveis primitivas	
-					}	
-
+						linha = linha.trim(); // Limpa a identação.
+						lePrimitivos.idLinha(linha,0); // Chama o metodo para ler as variaveis primitivas.
+					}
+                    // Caso contrário, se a linha não possuir nem chaves, nem for comentada e seu tamanho for diferente de zero:
 					else if( !tiraEspacos(linha).contains("{") && !tiraEspacos(linha).contains("}") && (!linha.contains("//")) && tiraEspacos(linha).length() != 0 ) 
 					{
-						
+                        // Acessa a mensagem de erro de código zero e desativa o laço.
 						ErrosNaCompilacao.getLineError(0);
 						laco = false;
 					}
-					
-					ErrosNaCompilacao.numeroDaLinha+=1;
-                		
-				}	
+					ErrosNaCompilacao.numeroDaLinha+=1; // COMO FUNCIONA ISSO DAQUI?
+				}
+                // Caso contrário, termina o laço.	
 				else 
 				{
-					break;
+					break; 
 				}
-
 			}
-
+            // No fim do laço, fecha o buffer de leitura.
 			buffRead.close();
 		}
-		
+		// Caso ocorra um erro durante a execução do código, o mesmo é impresso na tela
 		catch(IOException e)
 		{
 			e.printStackTrace();
 		}
 	}
-	//fim do método reader
+	//fim do método reader.
 	
 	//inicio do método getArquivo
 	public String getArquivo()
 	{
-		return this.arquivo;
+		return this.arquivo; // ACESSA O CAMINHO DO ARQUIVO A SER LIDO?
 	}
 	//fim do método getArquivo
 	
 	//inicio do método setArquivo
 	public void setArquivo(String a)
 	{
-		this.arquivo = a;	
+		this.arquivo = a; // Coloca o caminho do arquivo a ser lido na variável
 	}
 	//fim do método setArquivo
 }
