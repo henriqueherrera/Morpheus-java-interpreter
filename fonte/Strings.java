c33b3ca6252a624c7d4e88c465b423ffae3fc884
PabloLodi <lodiricardopablo@gmail.com>
2019-05-22 23:27:26 -0300

    corrigida a identação do Strings.java

--- fonte/Strings.java
+++ fonte/Strings.java
@@ -3,22 +3,22 @@ import java.util.HashMap;
 class Strings extends Variaveis
 {
 	private Int inteiro;
-
 	public static HashMap<String,String> variaveisArmazenadas = new HashMap<String, String>();
-
 	public static HashMap<String,HashMap<Integer,String>> vetoresArmazenados = new HashMap<String,HashMap<Integer,String>>();
-	
+
 	protected void indentificaValor(String str, int pos)
 	{
 		this.valueString(str,pos);
 	}
+
 	@Override
 	protected void inserirVariavel(String nomeVetor, int indice, String id, String index) //inseri variavel no vetor
-	{		
+	{
 		Int inteiro = new Int();
 		String val= vetoresArmazenados.get(nomeVetor).get(indice);
 		vetoresArmazenados.get(id).put(inteiro.indetificadorDeNumerosInt(index,0),val);
 	}
+
 	@Override
 	protected void inserirValor(String id,String index,String value) //inseri valor no vetor
 	{
@@ -32,11 +32,14 @@ class Strings extends Variaveis
 	{
 		vetoresArmazenados.put(id, new HashMap<Integer,String>());
 		Comparadores.tipoVariaveis.put(id, "string");
+
 		for(int i = 0; i < size ;i++)
 		{
 			vetoresArmazenados.get(id).put(i," ");
 		}
-	}	
+
+	}
+
 	@Override
 	//armazena o nome e o valor das variaveis no hasmap
 	protected void armazenarValor()
@@ -44,8 +47,8 @@ class Strings extends Variaveis
 		variaveisArmazenadas.put(this.getVariavelId(), this.getVariavelValue());
 		Comparadores.tipoVariaveis.put(this.getVariavelId(),"string");
 	}
-	
-	//inicio do método valueString	
+
+	//inicio do método valueString
 	public void valueString(String str, int pos)
 	{
 		boolean fimDaString = false;
@@ -55,29 +58,33 @@ class Strings extends Variaveis
 			//verifica se se tem aspas e continua a partir delas
 			if( str.charAt(i) == '"')
 			{
-				//laco para ler oque esta dentro das aspas	
+				//laco para ler oque esta dentro das aspas
 				for(int ii = i+1; ii< str.length()-1; ii++)
 				{
 					//verifica as aspas finais e para a verificação
 					if(str.charAt(ii) == '"')
 					{
-						
+
 						fimDaString = true;
 						break;
 					}
 					else
 					{
 
-						//seta a string que vai estar relacionada com a variavel 
-						setVariavelValue(getVariavelValue()+""+str.charAt(ii));  
+						//seta a string que vai estar relacionada com a variavel
+						setVariavelValue(getVariavelValue()+""+str.charAt(ii));
 					}
+
 				}
+
 			}
 			else if(fimDaString)
 			{
 				break;
 			}
+
 		}
+		
 	}
 	//fim do método valueString
 }
