7fdfe315fed1a685fe96103a813a9a830c3c9a30
PabloLodi <lodiricardopablo@gmail.com>
2019-05-22 22:40:33 -0300

    melhorei o aspecto visual do arquivo Condicional.java

--- fonte/Condicional.java
+++ fonte/Condicional.java
@@ -2,23 +2,26 @@
 public class Condicional extends Tools
 {
 	//inicio do método laco
-	public int linhaDoElse = -1; // se for diferente de -1 é pq tem um else
+	public int linhaDoElse = -1; // Se for diferente de -1 é porque existe um else
 
 	public void condicionalIf(int numeroDeLinhas, String value)
-    {
+  {
 		int end = numeroDeLinhas;
 		String tipo = "";
+
 		if(linhaDoElse != -1) // se nao tiver else o if le todas as linhas se tiver ele le ate o ponto em que esta o else
 		//que seria o linhaDoElse
 		{
 			end = linhaDoElse;
 		}
-		Comparadores testBooleano = new Comparadores();
+
+		Comparadores testBooleano = new Comparadores(); // Instancia o objeto de comparação
+
 		for(int i = 0; i < value.length(); i++)
 		{
 			if(value.charAt(i) == '|')
 			{
-				value = value.substring(i+1,value.length()); //le a expressao ou variavel
+				value = value.substring(i+1,value.length()); // Le a expressao ou variavel
 				break;
 			}
 
@@ -27,127 +30,155 @@ public class Condicional extends Tools
 				tipo = tipo+""+value.charAt(i);
 			}
 		}
-		
+
 		if(tipo.equals("string"))
 		{
+
 			if(testBooleano.comparaStr(value))
-            {
-				le = new LePrimitivos();	
+	    {
+				le = new LePrimitivos();
 				for(int i = 1; i<=end;i++)
 				{
 					le.idLinha(txtLines.get(i),i);
-                }
+	      }
 			}
+
 			else if(this.linhaDoElse > -1)
 			{
-				le = new LePrimitivos();				
+				le = new LePrimitivos();
 				for(int i = this.linhaDoElse; i<= numeroDeLinhas; i++)
 				{
 					le.idLinha(txtLines.get(i),i);
 				}
 			}
+
 		}
 
 		else if(tipo.equals("bool"))
 		{
+
 			if (value.contains("[") && value.contains("]"))
-			{	
+			{
 				value = testBooleano.valorDoVetor(value);
+
 				if( Boolean.parseBoolean(value))
 				{
-					le = new LePrimitivos();	
+					le = new LePrimitivos();
 					for(int i = 1; i<=end;i++)
 					{
 						le.idLinha(txtLines.get(i),i);
-                	}
+	        }
 				}
+
 				else if(this.linhaDoElse > -1)
 				{
-					le = new LePrimitivos();				
+					le = new LePrimitivos();
 					for(int i = this.linhaDoElse; i<= numeroDeLinhas; i++)
 					{
 						le.idLinha(txtLines.get(i),i);
 					}
 				}
+
 			}
-            else if(Bool.variaveisArmazenadas.get(value))
-            {
-				le = new LePrimitivos();	
+      else if(Bool.variaveisArmazenadas.get(value))
+      {
+				le = new LePrimitivos();
+
 				for(int i = 1; i<=end;i++)
 				{
 					le.idLinha(txtLines.get(i),i);
-                }
+	      }
+
 			}
 			else if(this.linhaDoElse > -1)
 			{
-				le = new LePrimitivos();				
+				le = new LePrimitivos();
+
 				for(int i = this.linhaDoElse; i<= numeroDeLinhas; i++)
 				{
 					le.idLinha(txtLines.get(i),i);
 				}
+
 			}
 		}
-		
 		else if(tipo.equals("char"))
 		{
+
 			if(testBooleano.comparaChar(value))
-            {
-				le = new LePrimitivos();	
+	    {
+				le = new LePrimitivos();
+
 				for(int i = 1; i<=end;i++)
 				{
 					le.idLinha(txtLines.get(i),i);
-                }
+	      }
+
 			}
 			else if(this.linhaDoElse > -1)
 			{
-				le = new LePrimitivos();				
+				le = new LePrimitivos();
+
 				for(int i = this.linhaDoElse; i<= numeroDeLinhas; i++)
 				{
 					le.idLinha(txtLines.get(i),i);
 				}
+
 			}
+
 		}
 
 		else if (tipo.equals("int"))
 		{
 
 			if(testBooleano.comparaInt(value))
-            {
-				le = new LePrimitivos();	
+	    {
+				le = new LePrimitivos();
+
 				for(int i = 1; i<=end;i++)
 				{
 					le.idLinha(txtLines.get(i),i);
-                }
+	      }
+
 			}
 			else if(this.linhaDoElse > -1)
 			{
-				le = new LePrimitivos();				
+				le = new LePrimitivos();
+
 				for(int i = this.linhaDoElse; i<= numeroDeLinhas; i++)
 				{
 					le.idLinha(txtLines.get(i),i);
 				}
+
 			}
+
 		}
 
 		else if (tipo.equals("double"))
 		{
+
 			if(testBooleano.comparaDouble(value))
 			{
-				le = new LePrimitivos();	
+				le = new LePrimitivos();
+
 				for(int i = 1; i<=end;i++)
 				{
 					le.idLinha(txtLines.get(i),i);
-                }
+	      }
+
 			}
 			else if(this.linhaDoElse > -1)
 			{
-				le = new LePrimitivos();				
+				le = new LePrimitivos();
+
 				for(int i = this.linhaDoElse; i<= numeroDeLinhas; i++)
 				{
 					le.idLinha(txtLines.get(i),i);
 				}
+
 			}
+
 		}
+		
 	}
 	//fim do método lacoDeRepeticao
 }
