1d1e808fd398f3bf2eabc4bc85c7092c754e0832
PabloLodi <lodiricardopablo@gmail.com>
2019-05-22 22:43:14 -0300

    melhorei o aspecto visual do arquivo Char.java

--- fonte/Char.java
+++ fonte/Char.java
@@ -3,15 +3,17 @@ import java.util.HashMap;
 //inicio da classe Char
 public class Char extends Variaveis
 {
+
 	Int inteiro;
+
 	//dicionario que armazena as váriaveis criadas
 	public static HashMap< String,Character> variaveisArmazenadas = new HashMap<String, Character>();
-	
+
 	public static HashMap< String, HashMap<Integer,Character>> vetoresArmazenados = new HashMap<String, HashMap<Integer,Character>>();
-	
+
 	@Override
 	protected void inserirVariavel(String nomeVetor, int indice, String id, String index) //inseri variavel no vetor
-	{		
+	{
 		char val= vetoresArmazenados.get(nomeVetor).get(indice);
 		Int inteiro = new Int();
 		vetoresArmazenados.get(id).put(inteiro.indetificadorDeNumerosInt(index,0),val);
@@ -29,39 +31,44 @@ public class Char extends Variaveis
 	{
 		vetoresArmazenados.put(id, new HashMap<Integer,Character>());
 		Comparadores.tipoVariaveis.put(id, "char");
+
 		for(int i = 0; i < size ;i++)
 		{
 			vetoresArmazenados.get(id).put(i,' ');
 		}
-	}	
-	
+
+	}
+
 	@Override
-	protected void indentificaValor (String str,int pos) 
+	protected void indentificaValor (String str,int pos)
 	{
+
 		for(int iii = pos; iii<str.length(); iii++)
 		{
+
 			if ( (str.charAt(iii) == 39) && !(iii+2 == str.length()-1))
-				{
+			{
 
 				if(str.charAt(iii+2) == 39)
 				{
-						
 					this.setCharValue(str.charAt(iii+1));
 					setVariavelValue(""+this.getCharValue());
 					break;
 				}
+
 			}
 
 		}
-				
 		this.setCharId(getVariavelId());
+
 	}
-	
+
 	@Override
 	protected void armazenarValor ()
 	{
 		variaveisArmazenadas.put(this.getCharId(), this.getCharValue());
 		Comparadores.tipoVariaveis.put(this.getCharId(), "char");
 	}
+	
 }
 //fim da classe Char
