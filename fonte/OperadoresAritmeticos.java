973426134c8de8bca24a2b194edcb9ea3ece6758
PabloLodi <lodiricardopablo@gmail.com>
2019-05-22 23:30:38 -0300

    corrigida a identação do OperadoresAritmeticos.java

--- fonte/OperadoresAritmeticos.java
+++ fonte/OperadoresAritmeticos.java
@@ -4,15 +4,10 @@ import java.util.ArrayList;
 public class OperadoresAritmeticos
 {
 	private ArrayList<String> heap = new ArrayList<String>(); //pseudo-árvore heap
-	
 	private Int inteiro = new Int();
-
-	private int inicioDaExpressao; 
-
+	private int inicioDaExpressao;
 	private int fimDaExpressao;
-	
 	private String  variavelParaArmazenar;
-
 	private Doubles doubles = new Doubles();
 
 	//inicio do método setVariavelParaArmazenar
@@ -21,66 +16,67 @@ public class OperadoresAritmeticos
 		this.variavelParaArmazenar = str;
 	}
 	//fim do método setVariavelParaArmazenar
-	
+
 	//inicio do método getVariavelParaArmazenar
 	public String getVariavelParaArmazenar()
 	{
 		return this.variavelParaArmazenar;
 	}
 	//fim do método getVariavelParaArmazenar
-	
+
 	//inicio do método setInicioDaExpressao
 	public void setInicioDaExpressao(int i)
 	{
 		this.inicioDaExpressao = i;
 	}
 	//fim do método setInicioDaExpressao
-	
+
 	//inicio do método getInicioDaExpressao
 	public int getInicioDaExpressao()
 	{
 		return this.inicioDaExpressao;
 	}
 	//fim do método getInicioDaExpressao
-	
+
 	//inicio do método setFimDaExpressao
 	public void setFimDaExpressao(int i)
 	{
 		this.fimDaExpressao = i;
 	}
 	//fim do método setFimDaExpressao
-	
+
 	//inicio do método getFimDaExpressao
 	public int getFimDaExpressao()
 	{
 		return this.fimDaExpressao;
 	}
 	//fim do método getFimDaExpressao
-	
+
 	//inicio do método left
 	private int left(int i)
 	{
 		return 2*i;
 	}
 	//fim do método left
-	
+
 	//inicio do método right
 	private int right(int i)
 	{
 		return 2*i+1;
 	}
 	//fim do método right
-	
+
 	//inicio do método dad
 	private int dad(int i)
 	{
 		return i/2;
 	}
 	//fim do método dad
-	
+
 	//inicio do método isLeaf
 	public boolean isLeaf(int i) //se o nó for folha ele retorna true
 	{
+
 		if(heap.get(left(i)).equals("empty") && heap.get(right(i)).equals("empty"))
 		{
 			return true;
@@ -89,12 +85,14 @@ public class OperadoresAritmeticos
 		{
 			return false;
 		}
+
 	}
 	//fim do método isLeaf
-	
+
 	//inicio do método ajuste
 	private void ajuste(int i)
 	{
+
 		if(heap.get(i) == "empty")
 		{
 			return;
@@ -107,20 +105,23 @@ public class OperadoresAritmeticos
 			heap.set(i,heap.get(left(i)));
 			heap.set(left(i),"empty");
 		}
-		
+
 		ajuste(this.right(i));
 	}
 	//fim do método ajuste
-	
+
 	//inicio do método val
 	public double val(int i) //aqui acontece a mágica, as continha são realizadas
 	{
+
 		if(isLeaf(i))
 		{
 			double x = 0;
 			doubles = new Doubles();
+
 			if(!((48<= heap.get(i).charAt(0)) && ( heap.get(i).charAt(0) <=57)))
 			{
+
 				if((Comparadores.tipoVariaveis.get(heap.get(i))).equals("int"))
 				{
 					x = Int.variaveisArmazenadas.get(heap.get(i));
@@ -130,25 +131,25 @@ public class OperadoresAritmeticos
 				{
 					x = Doubles.variaveisArmazenadas.get(heap.get(i));
 				}
+
 			}
 			else
 			{
 				if (heap.get(i).contains("."))
 				{
 					x = doubles.convertDouble(heap.get(i)+" ",0);
-			
 				}
 				else
 				{
 					x = inteiro.indetificadorDeNumerosInt(heap.get(i),0);
 				}
-				
+
 			}
 			return x;
 		}
 
 		if( heap.get(i).equals("+"))
-		{	
+		{
 			return val(left(i)) + val( right(i));
 		}
 
@@ -176,26 +177,29 @@ public class OperadoresAritmeticos
 		}
 	}
 	//fim do método val
-	
+
 	//inicio do analisadorLexicoDeOperacoes
 	public boolean analisadorLexicoDeOperacoes(String linha) //verifica se tem alguma expressao aritmetica
 	{
 		int total1 = linha.replaceAll("[^(]", "").length();
 		int total2 = linha.replaceAll("[^)]", "").length();
-	
+
 		if((linha.contains("+") || linha.contains("-") || linha.contains("*") || linha.contains("/") || linha.contains("%")) && (total1 == total2))
 		{
+
 			for(int i = 0; i<linha.length(); i++)
 			{
+
 				if (linha.charAt(i) == '(')
 				{
 					setInicioDaExpressao(i);
 					break;
 				}
+
 			}
 			setFimDaExpressao(linha.indexOf(";"));
 			setVariavelParaArmazenar(linha.substring(0,linha.indexOf("=")));
-			
+
 			return true;
 		}
 		else
@@ -204,36 +208,35 @@ public class OperadoresAritmeticos
 		}
 	}
 	//fim do método analisadorLexicoDeOperacoes
-	
+
 	//inicio do método leituraDaOperacao
 	public double leituraDaOperacao(String linha) // faz o cálculo da expressao
 	{
 		int indice = 1; //indice
-
 		String value = "";
-		
 		heap.add("x");
 
 		for(int i = 1; i< linha.length()*2; i++)
 		{
 			heap.add("empty");
 		}
+
 		for(int i = 0; i < linha.length() ; i++)
 		{
-			
+
 			if(linha.charAt(i) == '(')
 			{
 				heap.set(indice,"#");
 				indice = this.left(indice);
 			}
-			
+
 			else if(linha.charAt(i) == ')')
 			{
 				indice  = this.dad(indice);
 			}
-			
+
 			else if((linha.charAt(i) == '*') || (linha.charAt(i) == '-') || (linha.charAt(i) == '+') || (linha.charAt(i) == '/') ||(linha.charAt(i) == '%'))
-			{	
+			{
 				indice = this.dad(indice);
 				heap.set(indice,linha.substring(i,i+1));
 				indice = this.right(indice);
@@ -255,7 +258,7 @@ public class OperadoresAritmeticos
 						}
 					}
 				}
-				
+
 				else //se for variavel
 				{
 					for(int x = i; x<linha.length();x++)
@@ -276,36 +279,37 @@ public class OperadoresAritmeticos
 				{
 					value = valorDoVetor(value);
 				}
-				
 				heap.set(indice,value);
 				value = "";
+
 			}
+
 		}
-		
+
 		ajuste(1); //reorganiza a árvore caso necessário
 		return val(1);
 	}
 	//fim do método leituraDaOperacao
-	
+
     //inicio do método valorDoVetor
 	private String valorDoVetor(String value)
 	{
 		Int inteiro = new Int();
-		
 		String nomeVariavel = value.substring(value.indexOf("]")+1,value.length()); //pega o nome da variavel
-		
 		int index = inteiro.indetificadorDeNumerosInt(value.substring(value.indexOf("[")+1, value.indexOf("]")),0);
-	
+
 		if(Comparadores.tipoVariaveis.get(nomeVariavel).equals("int"))
 		{
 			return ""+Int.vetoresArmazenados.get(nomeVariavel).get(index);
 		}
 
-        else if(Comparadores.tipoVariaveis.get(nomeVariavel).equals("double"))
-        {
-            return ""+Doubles.vetoresArmazenados.get(nomeVariavel).get(index);
-        }
+    else if(Comparadores.tipoVariaveis.get(nomeVariavel).equals("double"))
+    {
+      return ""+Doubles.vetoresArmazenados.get(nomeVariavel).get(index);
+    }
 		return "";
+
 	}
+	
 }
 //fim da classe OperadoresAritmeticos
