6dcfd9ee39df19a1166eebc8892a9db623a3d279
PabloLodi <lodiricardopablo@gmail.com>
2019-05-22 22:32:56 -0300

    melhorei o visual do arquivo Leitor

--- fonte/Leitor.java
+++ fonte/Leitor.java
@@ -5,9 +5,9 @@ class Leitor extends Tools
 	private String arquivo; // Le o arquivo para ser Interpretado.
 
 	private LePrimitivos lePrimitivos = new LePrimitivos(); // Instancia o objeto que controla a leitura dos tipos primitivos.
-	
+
 	public static boolean laco = true; // Enquanto tudo estiver ocorrendo de maneira prevista
-					// o laco de repeticao continua trocando de linha.
+																		 // o laco de repeticao continua trocando de linha.
 	//Construtor
 	public Leitor(String arq) // Construtor que define o caminho do arquivo.
 	{
@@ -23,49 +23,47 @@ class Leitor extends Tools
 		}
 	}
 
-	public Leitor() // PRA QUE SERVE ESSE CONSTRUTOR SOZINHO?
+	public Leitor() // Ajuste técnico que evita problemas quando uma condição tenta instancia-lo
 	{
 	}
 
 	//inicio do método reader.
-	public void reader() 
+	public void reader()
 	{
 		// Tenta rodar os comandos seguintes:
 		try
 		{
 			BufferedReader buffRead = new BufferedReader(new FileReader (this.getArquivo())); // Le o arquivo do código.
+			String linha = ""; // Define a variável que armazena cada linha lida.
+    	// Enquanto tudo estiver rodando corretamente, o laço permanece rodando.
+    	while(laco)
+    	{
+    		linha = buffRead.readLine(); // Guarda a linha atual na variável.
 
-            		String linha = ""; // Define a variável que armazena cada linha lida.
-		    	// Enquanto tudo estiver rodando corretamente, o laço permanece rodando.
-            		while(laco)
-            		{
-		
-		    		linha = buffRead.readLine(); // Guarda a linha atual na variável.
-           
-				if(linha != null) // Se a linha não for nula:
-                		{
-                    			// Se a linha for diferente de zero e não estiver comentada:
-					if((linha.length() != 0 ) && (!linha.contains("//")) && (!linha.trim().equals("")))
-					{
-						linha = linha.trim(); // Limpa a identação.
-						lePrimitivos.idLinha(linha,0); // Chama o metodo para ler as variaveis primitivas.
-					}
-                    			// Caso contrário, se a linha não possuir nem chaves, nem for comentada e seu tamanho for diferente de zero:
-					else if( !tiraEspacos(linha).contains("{") && !tiraEspacos(linha).contains("}") && (!linha.contains("//")) && !tiraEspacos(linha).equals("") && tiraEspacos(linha).length() == 0) 
-					{
-                        			// Acessa a mensagem de erro de código zero e desativa o laço.
+		    if(linha != null) // Se a linha não for nula:
+        {
+          // Se a linha for diferente de zero e não estiver comentada:
+			    if((linha.length() != 0 ) && (!linha.contains("//")) && (!linha.trim().equals("")))
+			    {
+				    linha = linha.trim(); // Limpa a identação.
+				    lePrimitivos.idLinha(linha,0); // Chama o metodo para ler as variaveis primitivas.
+			    }
+          // Caso contrário, se a linha não possuir nem chaves, nem for comentada e seu tamanho for diferente de zero:
+			    else if( !tiraEspacos(linha).contains("{") && !tiraEspacos(linha).contains("}") && (!linha.contains("//")) && !tiraEspacos(linha).equals("") && tiraEspacos(linha).length() == 0)
+			    {
+            // Acessa a mensagem de erro de código zero e desativa o laço.
 						ErrosNaCompilacao.getLineError(0);
 						laco = false;
 					}
 					ErrosNaCompilacao.numeroDaLinha+=1; // COMO FUNCIONA ISSO DAQUI?
 				}
-                // Caso contrário, termina o laço.	
-				else 
+        // Caso contrário, termina o laço.
+				else
 				{
-					break; 
+					break;
 				}
 			}
-            // No fim do laço, fecha o buffer de leitura.
+      // No fim do laço, fecha o buffer de leitura.
 			buffRead.close();
 		}
 		// Caso ocorra um erro durante a execução do código, o mesmo é impresso na tela
@@ -75,14 +73,14 @@ class Leitor extends Tools
 		}
 	}
 	//fim do método reader.
-	
+
 	//inicio do método getArquivo
 	public String getArquivo()
 	{
-		return this.arquivo; // ACESSA O CAMINHO DO ARQUIVO A SER LIDO?
+		return this.arquivo; // Retorna o nome do arquivo.
 	}
 	//fim do método getArquivo
-	
+
 	//inicio do método setArquivo
 	public void setArquivo(String a)
 	{
